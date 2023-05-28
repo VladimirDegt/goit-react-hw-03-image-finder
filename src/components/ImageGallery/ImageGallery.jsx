8933cf96393f39c76a7c2c6ccc0b53ba585148ca
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-28 18:40:12 +0300

    load more

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,8 +2,9 @@ import { Component } from "react";
 import { Spinner } from "spin.js";
 import { Notify } from 'notiflix';
 import { fetchImage } from "service/api-pixabay";
-import { StyledContainerGalerry, StyledItemGalerry, StyledImgGalerry } from "./ImageGallery.styled";
+import { StyledContainerGalerry, StyledItemGalerry, StyledImgGalerry, StyledButtonLoad } from "./ImageGallery.styled";
 import { Modal } from "components/Modal/Modal";
+import { Button } from "components/Button/Button";
 
 let spinner = new Spinner();
 
@@ -12,16 +13,28 @@ export class ImageGallery extends Component {
     inputValue: '',
     largeImageURL: '',
     alt: '',
+    pageNumber: 1,
+    images: []
   };
 
   componentDidUpdate(prevProps, _) {
     if(prevProps.inputValue !== this.props.inputValue){
         spinner.spin(document.querySelector('.centered-element'))
-        fetchImage(this.props.inputValue)
+        this.setState({
+          pageNumber: 1
+        });
+        
+        fetchImage(this.props.inputValue, 1)
           .then((images)=>
-          {this.setState({inputValue: images.data.hits})
+          {this.setState({
+            images: images.data.hits,
+            inputValue: this.props.inputValue,
+          })
           if(images.data.hits.length === 0){
             Notify.info('Пробачьте, по Вашему запиту нічого не знайдено!')
+            this.setState({
+              inputValue: '',
+              images: []})
           }
           })
           .catch((error)=>{
@@ -38,12 +51,37 @@ export class ImageGallery extends Component {
     this.setState({largeImageURL, alt})
   };
 
+  handlerBtnClick = () => {
+    if(this.state.inputValue === this.props.inputValue) {
+      const pageNumber = this.state.pageNumber + 1
+    
+    fetchImage(this.state.inputValue, pageNumber)
+      .then((images)=>
+      {this.setState({
+        images: [...this.state.images, ...images.data.hits],
+      })
+      })
+      .catch((error)=>{
+        Notify.failure('Щось пішло не так!')
+        console.log(error)
+      })
+      .finally(()=>{
+        spinner.stop();
+      })
+
+    this.setState({
+      pageNumber,
+    })  
+  };
+};
+
   render(){
-    const {inputValue, largeImageURL, alt} = this.state;
+    const {inputValue, largeImageURL, alt, images} = this.state;
 
     return (
-      <StyledContainerGalerry>
-        {inputValue && inputValue.map((item)=>{
+      <>
+      <StyledContainerGalerry>  
+        {inputValue && images.map((item)=>{
           return (
             <StyledItemGalerry key={item.id}>
               <StyledImgGalerry 
@@ -59,5 +97,8 @@ export class ImageGallery extends Component {
         alt= {alt}
         />}
       </StyledContainerGalerry>
+        {inputValue && <StyledButtonLoad type="button" onClick={this.handlerBtnClick}>Load more</StyledButtonLoad>}
+        </>
     )}
   }; 
+
